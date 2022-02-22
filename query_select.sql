963a3e9fbbe8e478b462e784548b41e1d6b5a55e
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-22 17:28:13 +0100

    fixed like operators using equal operators

--- query_select.sql
+++ query_select.sql
@@ -8,13 +8,13 @@ SELECT * FROM `courses` WHERE `cfu`> 10 ORDER BY `cfu`;
 SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992 ORDER BY YEAR(`date_of_birth`); 
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
-SELECT * FROM `courses` WHERE `period` LIKE "I semestre" AND `year`= 1; 
+SELECT * FROM `courses` WHERE `period` = "I semestre" AND `year`= 1; 
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`) > 13 ORDER BY `hour`;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
-SELECT * FROM `degrees` WHERE `level` LIKE "magistrale";
+SELECT * FROM `degrees` WHERE `level` = "magistrale";
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
 SELECT COUNT(`id`) FROM `departments`;
