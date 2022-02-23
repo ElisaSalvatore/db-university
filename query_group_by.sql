a1867b16532f7de65e0eada95ca34e8548782579
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-23 18:38:21 +0100

    fixed code identation

--- query_group_by.sql
+++ query_group_by.sql
@@ -1,11 +1,22 @@
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-SELECT YEAR(`enrolment_date`), COUNT(`id`) AS "ID_Studenti_iscritti" FROM `students` GROUP BY YEAR(`enrolment_date`) ORDER BY YEAR(`enrolment_date`);
+SELECT YEAR(`enrolment_date`), COUNT(`id`) AS "ID_Studenti_iscritti" 
+FROM `students` GROUP BY YEAR(`enrolment_date`) 
+ORDER BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT COUNT(`teachers`.`id`), `office_address` FROM `teachers` GROUP BY `office_address`;
+SELECT COUNT(`teachers`.`id`), `office_address` 
+FROM `teachers` 
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-SELECT `exam_id` AS "ID_Appello", `exams`.`date` AS "data_appello", `exams`.`hour` AS "ora_appello", AVG(`vote`) AS "media_appello" FROM `exam_student` JOIN `exams` ON `exam_id` = `exams`.`id` GROUP BY `exams`.`date`, `exams`.`hour`, `exam_id` ORDER BY data_appello, ora_appello;
+SELECT `exam_id` AS "ID_Appello", `exams`.`date` AS "data_appello", `exams`.`hour` AS "ora_appello", 
+        AVG(`vote`) AS "media_appello" 
+FROM `exam_student` JOIN `exams` ON `exam_id` = `exams`.`id` 
+GROUP BY `exams`.`date`, `exams`.`hour`, `exam_id` 
+ORDER BY data_appello, ora_appello;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
-SELECT COUNT(`degrees`.`id`) AS "ID_Corso_di_laurea", `department_id` AS "ID_(in)Dipartimento" FROM `degrees` GROUP BY `department_id`;
+SELECT COUNT(`degrees`.`id`) AS "ID_Corso_di_laurea", 
+    `department_id` AS "ID_(in)Dipartimento" 
+FROM `degrees` 
+GROUP BY `department_id`;
--- query_join.sql
+++ query_join.sql
@@ -1,17 +1,53 @@
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
-SELECT `students`.`id`, `degrees`.`name` FROM `students` INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id` WHERE `degrees`.`name` = "Corso di Laurea in Economia" ORDER BY `students`.`id`;
+SELECT `students`.`id`, `degrees`.`name` 
+FROM `students` 
+INNER JOIN `degrees` 
+    ON `students`.`degree_id` = `degrees`.`id` 
+WHERE `degrees`.`name` = "Corso di Laurea in Economia" 
+ORDER BY `students`.`id`;
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
-SELECT `degrees`.`id`AS "ID_Corso_laurea", `department_id`, `degrees`.`name` AS "nome_corso_laurea", `departments`.`id`AS "id_dipartimento", `departments`.`name` AS "nome_dipartimento" FROM `degrees` INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
+SELECT `degrees`.`id`AS "ID_Corso_laurea", `degrees`.`name` AS "nome_corso_laurea", 
+        `departments`.`id`AS "id_dipartimento", `departments`.`name` AS "nome_dipartimento" 
+FROM `degrees` 
+INNER JOIN `departments` 
+    ON `degrees`.`department_id` = `departments`.`id` 
+WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
-SELECT `teachers`.`id` AS "ID_Insegnante", `teachers`.`name`, `teachers`.`surname`, `courses`.`id` AS "ID_Corso", `courses`.`name` AS "nome_corso" FROM `course_teacher` INNER JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id` INNER JOIN `courses` ON `course_teacher`.`course_id` = `courses`.`id` WHERE `teacher_id` = 44;
+SELECT `teachers`.`id` AS "ID_Insegnante", `teachers`.`name`, `teachers`.`surname`, 
+        `courses`.`id` AS "ID_Corso", `courses`.`name` AS "nome_corso" FROM `course_teacher` 
+INNER JOIN `teachers` 
+    ON `course_teacher`.`teacher_id` = `teachers`.`id` 
+INNER JOIN `courses` 
+    ON `course_teacher`.`course_id` = `courses`.`id` 
+WHERE `teacher_id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-SELECT `students`.`id` AS "ID_Studente",`students`.`surname`, `students`.`name`, `departments`.`id` AS "ID_Dipartimento",`departments`.`name`, `degrees`.`id` AS "ID_Corso_laurea", `degrees`.`name`, `degrees`.`level`, `degrees`.`address`, `degrees`.`email`, `degrees`.`website` FROM `degrees` INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id` INNER JOIN `students` ON `students`.`degree_id` = `degrees`.`id` ORDER BY `students`.`surname`, `students`.`name`;
+SELECT `students`.`id` AS "ID_Studente",`students`.`surname`, `students`.`name`, 
+        `departments`.`id` AS "ID_Dipartimento",`departments`.`name`, 
+        `degrees`.`id` AS "ID_Corso_laurea", `degrees`.`name`, `degrees`.`level`, `degrees`.`address`, `degrees`.`email`, `degrees`.`website` 
+FROM `degrees` 
+INNER JOIN `departments` 
+    ON `degrees`.`department_id` = `departments`.`id` 
+INNER JOIN `students` 
+ON `students`.`degree_id` = `degrees`.`id` 
+ORDER BY `students`.`surname`, `students`.`name`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-SELECT `degrees`.`id` AS "ID_Corso_Laurea",`degrees`.`name` AS "nome_corso_laurea", `courses`.`id` AS "ID_Corso",`courses`.`name` AS "nome_corso", `teachers`.`id` AS "ID_Insegnante",`teachers`.`name`,`teachers`.`surname` FROM `degrees` INNER JOIN `courses` ON `degrees`.`id` = `courses`.`degree_id` INNER JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`teacher_id` INNER JOIN `teachers` ON `course_teacher`.`teacher_id`= `teachers`.`id` GROUP BY `degrees`.`id`,`degrees`.`name`, `courses`.`id` ,`courses`.`name`, `teachers`.`id` ,`teachers`.`name`,`teachers`.`surname`;
+SELECT `degrees`.`id` AS "ID_Corso_Laurea",`degrees`.`name` AS "nome_corso_laurea", 
+        `courses`.`id` AS "ID_Corso",`courses`.`name` AS "nome_corso", 
+        `teachers`.`id` AS "ID_Insegnante",`teachers`.`name`,`teachers`.`surname` 
+FROM `degrees` 
+INNER JOIN `courses` 
+    ON `degrees`.`id` = `courses`.`degree_id` 
+INNER JOIN `course_teacher` 
+    ON `courses`.`id` = `course_teacher`.`teacher_id` 
+INNER JOIN `teachers` 
+    ON `course_teacher`.`teacher_id`= `teachers`.`id` 
+GROUP BY `degrees`.`id`,`degrees`.`name`, 
+        `courses`.`id` ,`courses`.`name`, 
+        `teachers`.`id` ,`teachers`.`name`,`teachers`.`surname`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 SELECT `departments`.`id` AS "ID_Dipartimento", `departments`.`name`,
