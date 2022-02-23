3111c1c1fa21c492325718d0b0c6595621604e8a
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-23 14:25:08 +0100

    added some queries to filter by students age

--- query_select.sql
+++ query_select.sql
@@ -5,13 +5,16 @@ SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990 ORDER BY MONTH(`date
 SELECT * FROM `courses` WHERE `cfu`> 10 ORDER BY `cfu`;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992 ORDER BY YEAR(`date_of_birth`); 
+-- SELECT * FROM `students` WHERE YEAR(`date_of_birth`) < 1992 ORDER BY YEAR(`date_of_birth`); 
+-- SELECT *, DATE_SUB(CURDATE(), INTERVAL 30 YEAR) FROM `students` WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
+SELECT *, TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) FROM `students` WHERE TIMESTAMPDIFF(YEAR, `date_of_birth`, CURDATE()) > 30;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * FROM `courses` WHERE `period` = "I semestre" AND `year`= 1; 
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`) > 13 ORDER BY `hour`;
+-- SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND HOUR(`hour`) >= 14 ORDER BY `hour`;
+SELECT * FROM `exams` WHERE `date` = '2020-06-20' AND `hour` > "14:00:00" ORDER BY `hour`;
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 SELECT * FROM `degrees` WHERE `level` = "magistrale";
