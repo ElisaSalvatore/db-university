bbec743594e87007e4911e7ae59e53b7fd6d69fa
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-24 15:04:44 +0100

    added some select statement examples and bonus methods

--- query_group_by.sql
+++ query_group_by.sql
@@ -4,16 +4,26 @@ FROM `students` GROUP BY YEAR(`enrolment_date`)
 ORDER BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-SELECT COUNT(`teachers`.`id`), `office_address` 
+SELECT `office_address`, COUNT(`teachers`.`id`)
 FROM `teachers` 
 GROUP BY `office_address`;
 
+-- Esempio usando HAVING
+-- SELECT `office_address`, COUNT(`teachers`.`id`) 
+-- FROM `teachers` 
+-- GROUP BY `office_address`
+-- 	HAVING COUNT(`teachers`.`id`) > 1;
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 SELECT `exam_id` AS "ID_Appello", `exams`.`date` AS "data_appello", `exams`.`hour` AS "ora_appello", 
-        AVG(`vote`) AS "media_appello" 
-FROM `exam_student` JOIN `exams` ON `exam_id` = `exams`.`id` 
+        ROUND(AVG(`vote`), 1) AS "media_voti_appello" 
+        -- ROUND AVG per arrotondare, mentre il numero "1" indica quante cifre decimali vogliamo mostrare (dopo la virgola)
+FROM `exam_student` 
+JOIN `exams` 
+    ON `exam_id` = `exams`.`id` 
 GROUP BY `exams`.`date`, `exams`.`hour`, `exam_id` 
-ORDER BY data_appello, ora_appello;
+    HAVING AVG(`vote`) > 20
+ORDER BY `data_appello`, `ora_appello`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
 SELECT COUNT(`degrees`.`id`) AS "ID_Corso_di_laurea", 
--- query_join.sql
+++ query_join.sql
@@ -8,11 +8,20 @@ ORDER BY `students`.`id`;
 
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
 SELECT `degrees`.`id`AS "ID_Corso_laurea", `degrees`.`name` AS "nome_corso_laurea", 
-        `departments`.`id`AS "id_dipartimento", `departments`.`name` AS "nome_dipartimento" 
+        `departments`.`id`AS "ID_Dipartimento", `departments`.`name` AS "nome_dipartimento" 
 FROM `degrees` 
 INNER JOIN `departments` 
     ON `degrees`.`department_id` = `departments`.`id` 
 WHERE `departments`.`name` = "Dipartimento di Neuroscienze";
+
+-- Altro esempio dove andiamo a filtrare solamente i corsi di laurea magistrali:
+-- SELECT `degrees`.`id`AS "ID_Corso_laurea", `degrees`.`name` AS "nome_corso_laurea", `degrees`.`level`, 
+--         `departments`.`id`AS "ID_Dipartimento", `departments`.`name` AS "nome_dipartimento" 
+-- FROM `degrees` 
+-- INNER JOIN `departments`
+--      ON `degrees`.`department_id` = `departments`.`id` 
+-- WHERE `departments`.`name` = "Dipartimento di Neuroscienze" 
+--     AND `degrees`.`level` = "magistrale" 
 
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 SELECT `teachers`.`id` AS "ID_Insegnante", `teachers`.`name`, `teachers`.`surname`, 
@@ -24,15 +33,15 @@ INNER JOIN `courses`
 WHERE `teacher_id` = 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
-SELECT `students`.`id` AS "ID_Studente",`students`.`surname`, `students`.`name`, 
+SELECT `students`.`id` AS "ID_Studente",`students`.`surname`, `students`.`name`, `students`.`registration_number`,
         `departments`.`id` AS "ID_Dipartimento",`departments`.`name`, 
         `degrees`.`id` AS "ID_Corso_laurea", `degrees`.`name`, `degrees`.`level`, `degrees`.`address`, `degrees`.`email`, `degrees`.`website` 
 FROM `degrees` 
 INNER JOIN `departments` 
     ON `degrees`.`department_id` = `departments`.`id` 
 INNER JOIN `students` 
-ON `students`.`degree_id` = `degrees`.`id` 
-ORDER BY `students`.`surname`, `students`.`name`;
+    ON `students`.`degree_id` = `degrees`.`id` 
+ORDER BY `students`.`surname`, `students`.`name`
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 SELECT `degrees`.`id` AS "ID_Corso_Laurea",`degrees`.`name` AS "nome_corso_laurea", 
@@ -72,4 +81,28 @@ WHERE `departments`.`name` = "Dipartimento di Matematica"
 ORDER BY `teachers`.`surname`, `teachers`.`name`;
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT `students`.`id` AS `ID_Studente`, `exams`.`course_id` AS `ID_Esame_materia`, 
+    COUNT(`exams`.`id`) AS `tentativi_esame`, 
+    MAX(`exam_student`.`vote`) AS `voto_max`
+FROM `students`
+INNER JOIN `exam_student`
+	ON `students`.`id` = `exam_student`.`student_id`
+INNER JOIN `exams`
+	ON `exam_student`.`exam_id` = `exams`.`id`
+GROUP BY `students`.`id`, `exams`.`course_id`
+	HAVING `voto_massimo` >= 18;
 
+-- Altro metodo per eseguire il bonus:
+    -- SELECT CONCAT(`students`.`surname`, " ", `students`.`name`) AS "studenti", `courses`.`name` AS "corso_esame", 
+    --     COUNT(`exam_student`.`exam_id`) AS "tentativi_esame", 
+    --     MAX(`exam_student`.`vote`) AS "voto_max" 
+    -- FROM `students` 
+    -- INNER JOIN `exam_student` 
+    --     ON `exam_student`.`student_id`=`students`.`id` 
+    -- INNER JOIN `exams` 
+    --     ON `exams`.`id`=`exam_student`.`exam_id` 
+    -- INNER JOIN `courses` 
+    --     ON `courses`.`id`= `exams`.`course_id` 
+    -- GROUP BY `Students`, `courses`.`name` 
+    --     HAVING MAX(`exam_student`.`vote`) > = 18 
+    -- ORDER BY `Students`
